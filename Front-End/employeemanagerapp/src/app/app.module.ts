504afed5152d9e50bff03f3a295e4ea662b3c14e
SabikunnaharPyaaasa <pyaasa15@gmail.com>
2022-01-23 21:50:17 +0600

    Add Employee done

--- Front-End/employeemanagerapp/src/app/app.module.ts
+++ Front-End/employeemanagerapp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -11,7 +12,7 @@ import { EmployeeService } from './employee.service';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule, FormsModule
   ],
   providers: [EmployeeService],
   bootstrap: [AppComponent]
