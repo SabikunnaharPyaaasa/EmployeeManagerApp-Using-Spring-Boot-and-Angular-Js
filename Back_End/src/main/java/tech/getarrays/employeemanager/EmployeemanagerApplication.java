d9d5d9a443c0c0cee49ec9178817c77267f3bcaa
SabikunnaharPyaaasa <pyaasa15@gmail.com>
2022-01-24 23:35:45 +0600

    back_end

---
+++ Back_End/src/main/java/tech/getarrays/employeemanager/EmployeemanagerApplication.java
@@ -0,0 +1,21 @@
+package tech.getarrays.employeemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class EmployeemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemanagerApplication.class, args);
+	}
+
+
+
+}
